444c8340c32b64c4b54a3f6912a3f4fce0e431bc
MRX <mrxmrx459@gmail.com>
2026-02-12 23:32:51 +0800

    fix(auth): change endpoints

--- web/UserAuth/app/login/page.tsx
+++ web/UserAuth/app/login/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
     setIsLoading(true);
 
     try {
-      const response = await fetch("http://localhost:8080/api/users/login", {
+      const response = await fetch("http://localhost:8080/api/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
