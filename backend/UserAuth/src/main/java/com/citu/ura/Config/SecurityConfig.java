3662f661fc27804126ed690e0f6680f12102aa07
MRX <mrxmrx459@gmail.com>
2026-02-12 23:30:18 +0800

    feat(security): permit access to auth endpoints

--- backend/UserAuth/src/main/java/com/citu/ura/Config/SecurityConfig.java
+++ backend/UserAuth/src/main/java/com/citu/ura/Config/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig {
             .csrf(csrf -> csrf.disable())
 
             .authorizeHttpRequests(auth -> auth
-                    .requestMatchers("/users/**").permitAll()
+                    .requestMatchers("/users/**", "/auth/**").permitAll()
                     .anyRequest().authenticated()
             )
 
