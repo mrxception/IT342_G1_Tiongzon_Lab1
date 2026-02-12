b790aeac2ab5376cc573b68898ac932902dcc6b4
MRX <mrxmrx459@gmail.com>
2026-02-12 21:43:07 +0800

    setup(project): setup springboot

---
+++ backend/UserAuth/src/test/java/com/citu/ura/UserAuthTests.java
@@ -0,0 +1,13 @@
+package com.citu.ura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAuthTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
