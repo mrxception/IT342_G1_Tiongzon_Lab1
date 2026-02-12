ff6f322b9b5830006ac0ab6f61c66fa7b89473fc
MRX <mrxmrx459@gmail.com>
2026-02-12 22:58:28 +0800

    feat(login): add login page

---
+++ web/UserAuth/app/login/page.tsx
@@ -0,0 +1,99 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useState } from "react";
+import Link from "next/link";
+
+export default function Home() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleLogin = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    try {
+      const response = await fetch("http://localhost:8080/api/users/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, password }),
+      });
+      
+      const data = await response.json()
+
+      if (response.ok) {
+        localStorage.setItem("token", data.token);
+        console.log(data.token);
+        window.location.href = "/profile"
+      } else {
+        alert(data.message);
+      }
+    }
+    catch (error) {
+      alert(error);
+    }
+    finally {
+      setIsLoading(false);
+    }
+  }
+
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <Card className="w-[350px] border border-gray-600 shadow-md">
+        <form onSubmit={handleLogin} className="space-y-5">
+          <CardHeader>
+            <CardTitle>Login</CardTitle>
+            <CardDescription>
+              Enter your credentials to access your account.
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="space-y-2">
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="username">Username</Label>
+              <Input
+                id="username"
+                placeholder="Type your username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)} 
+                required
+              />
+            </div>
+
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="password">Password</Label>
+              <Input
+                id="password"
+                type="password"
+                placeholder="Type your password"
+                value={password} 
+                onChange={(e) => setPassword(e.target.value)} 
+                required
+              />
+            </div>
+          </CardContent>
+          <CardFooter className="flex justify-end gap-x-2">
+            <Link href="/register">
+              <Button variant="outline">Sign up</Button>
+            </Link>
+            <Button type="submit" disabled={isLoading}>
+              {isLoading ? "Logging in..." : "Sign in"}
+            </Button>
+          </CardFooter>
+        </form>
+      </Card>
+    </div>
+  );
+}
